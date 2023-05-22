a78a3d63c42010e684327237c22ec6c71db31804
RASAMSETTI <meghana.rasamsetti@capgemini.com>
2023-05-22 12:17:34 +0530

    upadte project

--- Java Demo/src/com/examples/SortingHashMap.java
+++ Java Demo/src/com/examples/SortingHashMap.java
@@ -1,7 +1,7 @@
 package com.examples;
 
 import java.util.*;
-
+//new    jjhhh
 public class SortingHashMap {
 	public static void main(String[] args) {
 	Map<Integer,String> map=new HashMap<Integer,String>();
